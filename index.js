063178578b20141ecc97f46d53fdf131f8163359
Murtuzoff <r.murtuzoff@gmail.com>
2024-04-22 16:14:27 +0400

    some fixes

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const upload = multer({
   limits: { fileSize: 4 * 1024 * 1024 },
 });
 
+const fileSizeLimit = (req, res, next) => {
+  if (req.file && req.file.size > 4 * 1024 * 1024) {
+    return res.status(400).json({ error: "The file is larger than 4MB" });
+  }
+  next();
+};
+
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,28 +26,21 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
+app.post(
+  "/api/fileanalyse",
+  fileSizeLimit,
+  upload.single("upfile"),
+  (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const { originalname, mimetype, size } = req.file;
+    const fileInfo = { name: originalname, type: mimetype, size };
+
+    res.json(fileInfo);
   }
-
-  if (req.file.size > 4 * 1024 * 1024) {
-    return res.status(400).json({ error: "The file is larger than 4MB" });
-  }
-
-  const { originalname, mimetype, size } = req.file;
-  const fileInfo = { name: originalname, type: mimetype, size };
-
-  res.json(fileInfo);
-});
-
-app.use(function (err, req, res, next) {
-  if (err instanceof bodyParser.errors.PayloadTooLargeError) {
-    res.status(413).send("The request is too large");
-  } else {
-    next(err);
-  }
-});
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
